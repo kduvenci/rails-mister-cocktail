f1c01c31f2192f8b19c7e26bc3c9a8bdb2810d87
kemal duvenci <k.duvenci@gmail.com>
2018-11-10 01:01:10 +0900

    controllers & models completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,17 +1,18 @@
 class DosesController < ApplicationController
-
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    @collection = dose_collection(@cocktail)
   end
 
   def create
-    @ingredient = Ingredient.find(params[:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.ingredient = @ingredient
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to @dose
+      redirect_to cocktail_path(@cocktail)
     else
+      @collection = dose_collection(@cocktail)
       render :new
     end
   end
@@ -19,12 +20,20 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
+  def dose_collection(cocktail)
+    collection = Ingredient.order(:name)
+    cocktail.ingredients.each do |cocktail_ing|
+      collection = collection.reject { |collection_ing| collection_ing == cocktail_ing }
+    end
+    collection
+  end
 end
